40850696e29d40699ee3c08ba7560f162f325800
yuggof <yuggof0@gmail.com>
2016-09-14 19:52:22 +0000

    generated devise controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    sessions: 'users/sessions'
+  }
 
   resources :taxi_rides, only: %i[index new create]
 end
