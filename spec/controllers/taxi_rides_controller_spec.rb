4c9336eccca7b8df2868fd76bffacc8ca15e0111
yuggof <yuggof0@gmail.com>
2016-09-14 19:00:29 +0000

    taxi_rides#create spec

--- spec/controllers/taxi_rides_controller_spec.rb
+++ spec/controllers/taxi_rides_controller_spec.rb
@@ -52,16 +52,44 @@ RSpec.describe TaxiRidesController do
   describe 'POST #create' do
     context 'when authenticated' do
       context 'when params are valid' do
-        it 'redirects to index action'
+        it 'creates taxi ride' do
+          u = FactoryGirl.create(:user)
+          tp = FactoryGirl.create(:taxi_provider)
+
+          sign_in u
+          post :create, taxi_ride: {
+            start_address: 'Słomińskiego 1, Warszawa, Polska',
+            destination_address: 'Wiatraczna 1, Warszawa, Polska',
+            fare: '14.59',
+            taxi_provider_id: tp.id
+          }
+
+          expect(response).to have_http_status 302
+          expect(response).to redirect_to(taxi_rides_path)
+          expect(TaxiRide.count).to eql 1
+        end
       end
 
       context 'when params are invalid' do
-        it 'renders view'
+        it 'does not create taxi ride' do
+          u = FactoryGirl.create(:user)
+
+          sign_in u
+          post :create
+
+          expect(response).to have_http_status 200
+          expect(TaxiRide.count).to eql 0
+        end
       end
     end
 
     context 'when unauthenticated' do
-      it 'redirects to sign in page'
+      it 'redirects to sign in page' do
+        post :create
+
+        expect(response).to have_http_status 302
+        expect(response).to redirect_to(new_user_session_path)
+      end
     end
   end
 end
