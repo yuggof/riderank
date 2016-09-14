c84e24cc467cae133f9ec961708860c7cca6453c
yuggof <yuggof0@gmail.com>
2016-09-14 16:43:26 +0000

    taxi ride model validations

--- app/models/taxi_ride.rb
+++ app/models/taxi_ride.rb
@@ -1,2 +1,14 @@
 class TaxiRide < ApplicationRecord
+  belongs_to :taxi_provider
+
+  validates :start_address, presence: true
+  validates :destination_address, presence: true
+  validates :fare, presence: true, numericality: {
+    greater_than: 0
+  }
+  validates :taxi_provider_id, presence: true
+  validates :taxi_provider, presence: true
+  validates :distance, presence: true, numericality: {
+    only_integer: true, greater_than: 0
+  }
 end
