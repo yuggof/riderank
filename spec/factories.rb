785d97c80d651ed43c364a6910b0d0f107baf5f6
yuggof <yuggof0@gmail.com>
2016-09-14 16:50:24 +0000

    taxi ride factory

--- spec/factories.rb
+++ spec/factories.rb
@@ -2,4 +2,12 @@ FactoryGirl.define do
   factory :taxi_provider do
     sequence(:name) { |n| "taxi_provider_#{n}" }
   end
+
+  factory :taxi_ride do
+    sequence(:start_address) { |n| "Słomińskiego #{n}, Warszawa, Polska"  }
+    sequence(:destination_address) { |n| "Wiatraczna #{n}, Warszawa, Polska" }
+    fare { "#{1 + rand(100)}.#{rand(100)}" }
+    taxi_provider { create(:taxi_provider) }
+    distance { 1 + rand(10) }
+  end
 end
