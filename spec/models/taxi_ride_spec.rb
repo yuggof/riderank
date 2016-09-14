8333315b74fd7ded4e2b9f3c3925135863bf036b
yuggof <yuggof0@gmail.com>
2016-09-14 16:44:48 +0000

    taxi ride spec

--- spec/models/taxi_ride_spec.rb
+++ spec/models/taxi_ride_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
-RSpec.describe TaxiRide, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe TaxiRide do
+  it 'has a valid factory' do
+    expect(FactoryGirl.build(:taxi_ride)).to be_valid
+  end
 end
