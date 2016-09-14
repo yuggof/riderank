bbd29f924c537de491fcbf47802ea07650952248
yuggof <yuggof0@gmail.com>
2016-09-14 16:21:04 +0000

    added taxi providers seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+ApplicationRecord.transaction do
+  TaxiProvider.create!([
+    {name: 'Sawa'},
+    {name: 'Uber'},
+    {name: 'MPT'}
+  ])
+end
