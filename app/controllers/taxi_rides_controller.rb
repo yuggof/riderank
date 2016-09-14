4b78b12b6f4a6975be7a82ac25bb4dbc46a084c9
yuggof <yuggof0@gmail.com>
2016-09-14 18:51:20 +0000

    taxi_rides#index without view

---
+++ app/controllers/taxi_rides_controller.rb
@@ -0,0 +1,6 @@
+class TaxiRidesController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  devise_for :users
+
+  resources :taxi_rides, only: :index
 end
