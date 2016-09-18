aa2e0c924a13ac5436fc4e91b4cbfafe507db9f6
yuggof <yuggof0@gmail.com>
2016-09-18 20:22:45 +0000

    taxi ride distance calculation spec

--- spec/models/taxi_ride_spec.rb
+++ spec/models/taxi_ride_spec.rb
@@ -4,4 +4,34 @@ RSpec.describe TaxiRide do
   it 'has a valid factory' do
     expect(FactoryGirl.build(:taxi_ride)).to be_valid
   end
+
+  describe 'validation' do
+    context 'when distance between start address and destination address can be calculated' do
+      it 'sets distance' do
+        tr = TaxiRide.new(
+          start_address: 'a', destination_address: 'b',
+          fare: 1, taxi_provider: FactoryGirl.create(:taxi_provider)
+        )
+
+        expect(tr.valid?).to be true
+        expect(tr.distance).to eql 15063
+      end
+    end
+
+    context 'when distance between start address and destination address cannot be calculated' do
+      it 'sets errors on start_address and destination_address' do
+        tr = TaxiRide.new(
+          start_address: 'a', destination_address: 'b',
+          fare: 1, taxi_provider: FactoryGirl.create(:taxi_provider)
+        )
+
+        GoogleMapsDistanceMatrixAPI.client.distance_between_return_value = nil
+
+        expect(tr.valid?).to be false
+        expect(tr.errors[:start_address].size).to eql 1
+        expect(tr.errors[:destination_address].size).to eql 1
+        expect(tr.distance).to be nil
+      end
+    end
+  end
 end
