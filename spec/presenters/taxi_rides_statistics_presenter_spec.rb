5e0aaac03d2ed96345ab8eab7cedd7bc6cfdd56c
yuggof <yuggof0@gmail.com>
2016-09-19 18:56:58 +0000

    taxi_rides_statistics_presenter#kilometers_rode_during_current_week spec

---
+++ spec/presenters/taxi_rides_statistics_presenter_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe TaxiRidesStatisticsPresenter do
+  describe '#kilometers_rode_during_current_week' do
+    it 'returns kilometers rode during current week' do
+      t = Time.new(2016, 9, 19)
+
+      Timecop.freeze t do
+        trs = [
+          FactoryGirl.create(:taxi_ride, distance: 1, created_at: t),
+          FactoryGirl.create(:taxi_ride, distance: 2, created_at: t),
+          FactoryGirl.create(:taxi_ride, distance: 5, created_at: t + 1.day),
+          FactoryGirl.create(:taxi_ride, distance: 7, created_at: t + 3.days),
+          FactoryGirl.create(:taxi_ride, distance: 1000, created_at: t - 1.day),
+          FactoryGirl.create(:taxi_ride, distance: 1000, created_at: t + 1.week)
+        ]
+
+        trssp = TaxiRidesStatisticsPresenter.new(trs)
+
+        expect(trssp.kilometers_rode_during_current_week).to eql 15
+      end
+    end
+  end
+end
