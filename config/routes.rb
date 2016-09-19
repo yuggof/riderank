9f65a685985b96832c684b7f8b93f3d333581df5
yuggof <yuggof0@gmail.com>
2016-09-19 18:03:33 +0000

    redirect root to /taxi_rides

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  root to: redirect('/taxi_rides')
+
   devise_for :users, controllers: {
     registrations: 'users/registrations',
     sessions: 'users/sessions'
