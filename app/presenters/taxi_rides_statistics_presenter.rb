8c682bbc060addccb7d86c7b989dfd9c96beff63
yuggof <yuggof0@gmail.com>
2016-09-20 07:05:12 +0000

    taxi_rides_statistics_presenter#money_spent_during_current_week

--- app/presenters/taxi_rides_statistics_presenter.rb
+++ app/presenters/taxi_rides_statistics_presenter.rb
@@ -11,4 +11,13 @@ class TaxiRidesStatisticsPresenter
       .map(&:distance)
       .reduce(0, :+)
   end
+
+  def money_spent_during_current_week
+    n = Time.zone.now
+
+    @taxi_rides
+      .select { |tr| (n.beginning_of_week..n.end_of_week).cover?(tr.created_at) }
+      .map(&:fare)
+      .reduce(0, :+)
+  end
 end
