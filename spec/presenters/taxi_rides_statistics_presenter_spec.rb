9612810e83982ba97141e416920b642149c4ab3e
yuggof <yuggof0@gmail.com>
2016-09-20 07:04:18 +0000

    taxi_rides_statistics_presenter#money_spent_during_current_week spec

--- spec/presenters/taxi_rides_statistics_presenter_spec.rb
+++ spec/presenters/taxi_rides_statistics_presenter_spec.rb
@@ -31,4 +31,35 @@ RSpec.describe TaxiRidesStatisticsPresenter do
       end
     end
   end
+
+  describe '#money_spent_during_current_week' do
+    context 'when taxi rides are registered' do
+      it 'returns money spent during current week' do
+        t = Time.new(2016, 9, 19)
+
+        Timecop.freeze t do
+          trs = [
+            FactoryGirl.create(:taxi_ride, fare: 15, created_at: t),
+            FactoryGirl.create(:taxi_ride, fare: 2.5, created_at: t),
+            FactoryGirl.create(:taxi_ride, fare: 5.75, created_at: t + 1.day),
+            FactoryGirl.create(:taxi_ride, fare: 10.3, created_at: t + 3.days),
+            FactoryGirl.create(:taxi_ride, fare: 100, created_at: t - 1.day),
+            FactoryGirl.create(:taxi_ride, fare: 100, created_at: t + 1.week)
+          ]
+
+          trssp = TaxiRidesStatisticsPresenter.new(trs)
+
+          expect(trssp.money_spent_during_current_week).to eql 33.55
+        end
+      end
+    end
+
+    context 'when taxi rides are not registered' do
+      it 'returns 0' do
+        trssp = TaxiRidesStatisticsPresenter.new([])
+
+        expect(trssp.money_spent_during_current_week).to eql 0
+      end
+    end
+  end
 end
